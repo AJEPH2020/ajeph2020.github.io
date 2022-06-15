9304c42e78a418faa13e0f601b3117ac8a02c4a7
Atul Jeph <87131795+AJEPH2020@users.noreply.github.com>
2022-06-15 18:03:11 +0530

    16th update

--- home.js
+++ home.js
@@ -307,11 +307,11 @@ class Particle {
         if (ax != 0 || ay != 0) {
             var landscapeOrientation = window.innerWidth / window.innerHeight > 1;
             if (landscapeOrientation) {
-                this.directionX = this.directionX + ay/100000;
-                this.directionY = this.directionY + ax/100000;
+                this.directionX = this.directionX + ay/10000;
+                this.directionY = this.directionY + ax/10000;
             } else {
-                this.directionX = this.directionX - ax/100000;
-                this.directionY = this.directionY + ay/100000;
+                this.directionX = this.directionX - ax/10000;
+                this.directionY = this.directionY + ay/10000;
             }
         } else {
             this.directionY += 0.005;
